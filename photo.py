499f8c4163e5d67b921a9f259ac2f80dcc51a5f0
demidraksin <greatraksin@icloud.com>
2021-01-16 19:04:11 +0300

    Adding photo

---
+++ photo.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy
+import os
+
+
+face_file = 'haarcascade_frontalface_default.xml'
+dataset = 'datasets'
+subdata = 'sub'
+
+path = os.path.join(dataset, subdata)
+if not os.path.isdir(path):
+    os.mkdir(path)
+
+w, h = (500, 500)
+fCascade = cv2.CascadeClassifier(face_file)
+camera = cv2.VideoCapture(0)
+
+count = 1
+while count < 80:
+    index, image = camera.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = fCascade.detectMultiScale(gray, 1.6, 3)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 200), 4)
+        faces = gray[y:y+h, x:x+w]
+        faces_resize = cv2.resize()
